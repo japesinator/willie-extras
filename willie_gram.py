f2e46133889f04ea3f52a8de7386d2cc880a4f46
b0bby <unthinkingdepths+github@gmail.com>
2014-11-07 21:15:30 -0600

    Added anagram module

---
+++ willie_gram.py
@@ -0,0 +1,14 @@
+from willie import module
+import urllib.request
+import json
+
+@commands('anagram')
+@priority('low')
+def anagram(bot, trigger):
+    try:
+        url = "http://www.anagramica.com/best/:" + trigger
+        data = urllib.request.urlopen(url).read().decode("utf-8")
+        result = json.loads(data)
+        bot.say(result.get("best")[0])
+    except Exception as e:
+        bot.say('Sorry, I couldn\'t make an anagram out of that.')
