74078b54e296460df85b5f6e8d16b2759fec0fbf
Chris Higgins <chris@chigs.me>
2014-11-13 15:04:56 -0600

    Added the fuck module

---
+++ fuck.py
@@ -0,0 +1,14 @@
+"""
+fuck.py - Pretty obvious
+Copyright 2014, Chris Higgins http://chigstuff.com
+Licensed under the Eiffel Forum License 2.
+
+http://willie.dfbta.net
+"""
+import willie
+import random
+@willie.module.commands('fuck')
+def ball(bot, trigger):
+    bot.say("I'm a fucking twat...");
+
+    
