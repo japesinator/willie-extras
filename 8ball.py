a91ae578c4945289579e12f3b6cb73fe91085b00
JP Smith <jp.smith@ilstu.edu>
2014-11-15 16:11:43 -0600

    another day, another index fix

--- 8ball.py
+++ 8ball.py
@@ -10,8 +10,25 @@ import random
 @willie.module.commands('8')
 def ball(bot, trigger):
     """Ask the magic 8ball a question! Usage: .8 <question>"""
-    messages = ["It is certain"," It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","God says no","Very doubtful","Outlook not so good"]
-    answer = random.randint(0,len(messages))
-    bot.say(messages[answer]);
-
-    
+    messages = [ "It is certain"
+               , "It is decidedly so"
+               , "Without a doubt"
+               , "Yes definitely"
+               , "You may rely on it"
+               , "As I see it yes"
+               , "Most likely"
+               , "Outlook good"
+               , "Yes"
+               , "Signs point to yes"
+               , "Reply hazy try again"
+               , "Ask again later"
+               , "Better not tell you now"
+               , "Cannot predict now"
+               , "Concentrate and ask again"
+               , "Don't count on it"
+               , "My reply is no"
+               , "God says no"
+               , "Very doubtful"
+               , "Outlook not so good"
+               ]
+    bot.say(random.choice(messages));
